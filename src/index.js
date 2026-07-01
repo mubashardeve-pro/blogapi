dd18f1ca4d1509a0e92e70cb0e12de143c87b95b
mubashar <youremail@example.com>
2026-07-01 18:07:47 +0500

    Configure express to trust proxy and update cookie options for secure transmission.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.set("trust proxy", 1);
 const db = require('./db/models');
 const { categories } = require('./db/models');
 const globalErrorHandler = require('./utils/errorController');
--- src/utils/cookieOptions.js
+++ src/utils/cookieOptions.js
@@ -1,7 +1,8 @@
 const cookieOptions = {
   httpOnly: true,
+  secure: true, // https se hi cookie send karna hai
   sameSite: "lax",
-  maxAge: 24 * 60 * 60 * 1000,
+  maxAge: 24 * 60 * 60 * 1000, //  hai cookie ko store karna hai
 };
 
 module.exports = { cookieOptions };
